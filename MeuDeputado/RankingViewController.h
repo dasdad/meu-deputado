fa10dfd4c900d4c1479e67dedf31352d4686babd
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-04-20 00:19:11 -0300

    storyboard inicial

---
+++ MeuDeputado/RankingViewController.h
@@ -0,0 +1,13 @@
+//
+//  RankingViewController.h
+//  MeuDeputado
+//
+//  Created by rafael almeida on 19/04/13.
+//  Copyright (c) 2013 rafael almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RankingViewController : UITableViewController
+
+@end
